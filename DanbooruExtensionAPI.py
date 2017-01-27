398991a2f1efac5ab201dfdaf30a12a5a8a37ba1
luisfelipekai <luisfelipekai@gmail.com>
2017-01-27 19:11:15 -0300

    Check for the existence of the Path. Return proper response to the request. Comment the code.

--- DanbooruExtensionAPI.py
+++ DanbooruExtensionAPI.py
@@ -2,9 +2,8 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from bs4 import BeautifulSoup
 import urllib.request
+import json
 import os
-
-
 app = Flask(__name__)
 api = Api(app)
 
@@ -19,12 +18,16 @@ class DanbooruScraper(Resource):
         html = urllib.request.urlopen(url).read()
         soup = BeautifulSoup(html, 'html.parser')
         image_articles = soup.find_all("article")
+        # Retrieving urls for each image.
         for image_article in image_articles:
             image_urls.append(base_url + image_article['data-file-url'])
+            if not os.path.exists(path):
+                os.makedirs(path)
+        #Iterate over the urls list and downloading the images.
         for url in image_urls:
             image_name = url.split('/')[-1]
             urllib.request.urlretrieve(url, path + image_name)
-        return 200
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 api.add_resource(DanbooruScraper, '/api/download_images/<string:tags>')
 
